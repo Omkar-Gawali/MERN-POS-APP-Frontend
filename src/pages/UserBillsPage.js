bde7fbb100a48fdd3efed4c07c30c1ff8dc9b00a
Omkar-Gawali <omkargawali702@gmail.com>
2026-06-09 13:20:08 +0530

    UI improved

--- src/pages/UserBillsPage.js
+++ src/pages/UserBillsPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from "react";
 import { useReactToPrint } from "react-to-print";
 import LayOut from "../components/LayOut/LayOut";
